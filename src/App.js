57ac9268f2e9115538eabcac1d710cce1cc11076
Be10 <alexquirozaldave@gmail.com>
2022-08-23 12:18:53 -0500

    Commit 02

--- src/App.js
+++ src/App.js
@@ -19,14 +19,6 @@ const App = () => {
         <div className="container">
           <Routes>
             <Route exact path='/' element={<Home/>} />
-            <Route path='/Trabajadores' element={<Trabajadores/>} />
-            <Route path='/RadarGraph' element={<RadarGraph/>} />
-            <Route path='/Employees' element={<Employees/>} />
-            <Route path='/BestEmployeeTab' element={<BestEmployeeTab/>} />
-            <Route path='/EmployeeTab2' element={<EmployeeTab2/>} />
-            <Route path='/EmployeeTab3' element={<EmployeeTab3/>} />
-            <Route path='/EmployeeTab4' element={<EmployeeTab4/>} />
-            <Route path='/EmployeeTab5' element={<EmployeeTab5/>} />
           </Routes>
         </div>
     </Router>
